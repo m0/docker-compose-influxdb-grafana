17bad76eca0d60939f4eb23c0ea050b7835623c0
Moritz Maisel <maisel@sipgate.de>
2020-08-05 10:38:29 +0200

    doc: adds comment to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d
+# plugin provisioning is available from v7.1 but we use v7.0 so we script it here:
 docker-compose exec grafana grafana-cli plugins install agenty-flowcharting-panel
 docker-compose restart grafana
 docker-compose ps
